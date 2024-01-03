e359174a7c91b36487581c73bf34f4cbaede6a6c
linx-235 <xyfernx@gmail.com>
2024-01-03 12:03:40 -0500

    finished header
    i finised header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,14 @@ function Header() {
     return (
         <div className='header'>
             <div className="upper">
-                <div className="logo"></div>
-                <div className="background"></div>
+                <div className="logo">
+                    <img src={require("../images/logo.png")} alt="logo"></img>
+                </div>
+                <div className="background">
+                    <img src={require("../images/logobackground.png")} alt="logobackground"></img>
+                </div>
             </div>
-            <div className='curs'>
+            <div className='bottom curs'>
                 <ul>
                     <li className='active'><p>Home</p></li>
                     <li className='inactive'><p>Gallery</p></li>
