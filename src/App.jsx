6d83739393e1d56bd9df7b40d8a1f3a746983249
linx-235 <xyfernx@gmail.com>
2024-01-07 15:32:37 -0500

    added routing to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import { Route, Routes } from 'react-router-dom';
 
 import './styles/app.css';
 import Layout from "./components/Layout";
-import Header from './components/Header.jsx';
+// import Header from './components/Header.jsx';
 import Shop from './pages/Shop.jsx';
 import Home from './pages/Home.jsx';
+import Gallery from './pages/Gallery.jsx';
 import Contact from './pages/Contact.jsx';
 
 function App() {
@@ -13,6 +14,9 @@ function App() {
             <Route path="/" element={<Layout />}>
                 {/* PUBLIC ROUTES */}
                 <Route index element={<Home />} />
+                <Route path="gallery" element={<Gallery />} />
+                <Route path="shop" element={<Shop />} />
+                <Route path="contact" element={<Contact />} />
                 {/* END PUBLIC ROUTES */}
             </Route>
         </Routes>
---
+++ src/pages/Gallery.jsx
@@ -0,0 +1,12 @@
+import "../styles/theme.css";
+import "../styles/gallery.css";
+
+function Gallery() {
+    return (
+        <div className="">
+            <p>hi</p>
+        </div>
+    );
+}
+
+export default Gallery;
