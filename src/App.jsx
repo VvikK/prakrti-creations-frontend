6670340a3dd64d47c510de65bd65fce7cd18d302
SuperMK15 <smartiskush@gmail.com>
2024-01-07 15:10:17 -0500

    added basic routing and Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
+import { Route, Routes } from 'react-router-dom';
+
 import './styles/app.css';
+import Layout from "./components/Layout";
 import Header from './components/Header.jsx';
 import Shop from './pages/Shop.jsx';
 import Home from './pages/Home.jsx';
@@ -6,10 +9,13 @@ import Contact from './pages/Contact.jsx';
 
 function App() {
     return (
-        <div className='app'>
-            <Header />
-            <Contact />
-        </div>
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                {/* PUBLIC ROUTES */}
+                <Route index element={<Home />} />
+                {/* END PUBLIC ROUTES */}
+            </Route>
+        </Routes>
     );
 }
 
---
+++ src/components/Layout.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from 'react-router-dom'
+import Header from './Header.jsx'
+import Footer from './Footer.jsx'
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <div>
+        <Outlet />
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default Layout
