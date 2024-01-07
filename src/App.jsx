844ccf47a1bd5f349c51cfc8d709c60a63fe4da3
VvikK <78823821+VvikK@users.noreply.github.com>
2024-01-06 19:32:26 -0500

    commit message here

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import './styles/app.css';
 import Header from './components/Header.jsx';
 import Shop from './pages/Shop.jsx';
 import Home from './pages/Home.jsx';
+import Contact from './pages/Contact.jsx';
 
 function App() {
     return (
         <div className='app'>
             <Header />
-            <Home />
+            <Contact />
         </div>
     );
 }
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,16 @@
+import '../styles/contact.css'
+
+function Contact() {
+    return (
+        <div className="contact">
+            <div className="contact-info">
+                <h1>Contact Us</h1>
+                <p>Phone: 123-456-7890</p>
+                <p>Email: emailplaceholder@gmail.com</p>
+                <p>Instagram: @prakrti_kreations</p>
+            </div>
+        </div>
+    );
+}
+
+export default Contact;
