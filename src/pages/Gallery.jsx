9b39dd9b9f6c8f88f65a4dee0a5ca636f9589be7
George Fan <78384472+Wozunubi@users.noreply.github.com>
2024-01-08 23:07:41 -0500

    Gallery completed
    Added gallery that pulls images from the ../images/gallery folder

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import Masonry, {ResponsiveMasonry} from "react-responsive-masonry";
 
 import "../styles/theme.css";
 import "../styles/gallery.css";
 
 function Gallery() {
+    const context = require.context("../images/gallery", false, /\.(png|jpe?g|svg)$/);
+    const images = context.keys().map(image => context(image));
+
     return (
-        <div className="">
-            <p>hi</p>
+        <>
+        <div class="box">
+            <ResponsiveMasonry columnsCountBreakPoints={{350: 1, 600: 2, 900: 3, 1150: 4}}>
+                <Masonry gutter="20px">
+                    {images.map((image, i) => (<img key={i} src={image} style={{width: "100%", display: "block"}} alt="" onClick={null}/>))}
+                </Masonry>
+            </ResponsiveMasonry>
         </div>
+        </>
     );
-}
+};
 
 export default Gallery;
